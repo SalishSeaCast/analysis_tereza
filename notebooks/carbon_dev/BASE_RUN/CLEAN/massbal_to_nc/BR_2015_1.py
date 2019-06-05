7380d86652e30cfbdf186d14cf5186601a654eef
Tereza <tjarnik@gmail.com>
2019-06-05 13:45:40 -0700

    made ncmaker modular, calculated JS isopycnals

---
+++ notebooks/carbon_dev/BASE_RUN/CLEAN/massbal_to_nc/BR_2015_1.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from numpy import *
+from scipy import *
+import matplotlib.pyplot as plt
+import netCDF4 as nc
+import numpy as np
+import scipy as sp
+import seawater
+import datetime as dt
+""
+from salishsea_tools import (
+    nc_tools,
+    viz_tools,
+    geo_tools,
+    tidetools
+)
+import netCDF4 as nc
+import cmocean as cm
+import glob
+import sys
+sys.path.append('/data/tjarniko/mocsy')
+sys.path.append('/data/tjarniko/MEOPAR/at3/notebooks/carbon_dev/CCCmaDEV/CCCma_src')
+import mocsy
+import CCCma
+import CCCma_stations as cs
+from matplotlib import reload
+import arrow
+import gsw
+import MASSBAL_ncmaker as mb
+
+ncname_BR = 'MASSBAL_BR_2015_1_fullyear.nc'
+sdir = 'BR_1st_2015'
+start = '2015-01-01'
+end = '2015-12-31'
+st = dt.datetime(2015,1,1)
+en = dt.datetime(2015,12,31)
+calc_JS = 0
+mb.create_massbal_nc(ncname_BR, sdir, start, end, st, en, calc_JS)
