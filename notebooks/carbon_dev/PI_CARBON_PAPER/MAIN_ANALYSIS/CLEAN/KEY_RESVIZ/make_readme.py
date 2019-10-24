8d180cf7e0876fe12c96812b20692662c4a1fe06
Tereza <tjarnik@gmail.com>
2019-10-24 11:17:41 -0700

    read me

--- notebooks/carbon_dev/PI_CARBON_PAPER/MAIN_ANALYSIS/CLEAN/KEY_RESVIZ/make_readme.py
+++ notebooks/carbon_dev/PI_CARBON_PAPER/MAIN_ANALYSIS/CLEAN/KEY_RESVIZ/make_readme.py
@@ -23,8 +23,8 @@ import re
 
 
 NBVIEWER = 'http://nbviewer.jupyter.org/urls'
-REPO = 'bitbucket.org/salishsea/analysis-terez/raw/tip'
-REPO_DIR = 'notebooks'
+REPO = 'bitbucket.org/salishsea/analysis-tereza/raw/tip'
+REPO_DIR = 'notebooks/carbon_dev/PI_CARBON_PAPER/MAIN_ANALYSIS/CLEAN/KEY_RESVIZ/'
 TITLE_PATTERN = re.compile('#{1,6} ?')
 
 
